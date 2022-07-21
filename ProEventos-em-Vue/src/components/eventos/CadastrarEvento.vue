09c416af40d236a066f65c9106b8562ff78e2db4
Audrey  Ernesto de Lima <3970@ucdb.br>
2022-07-21 14:23:51 -0400

    criando validação de formulario

--- ProEventos-em-Vue/src/components/eventos/CadastrarEvento.vue
+++ ProEventos-em-Vue/src/components/eventos/CadastrarEvento.vue
@@ -5,22 +5,25 @@
                 <div>
                     <label class="label" for="username">Tema</label>
                     <div class="control has-icons">
-                        <input class="input" type="text" placeholder="Insira o Tema" />
+                        <input class="input" type="text" placeholder="Insira o Tema"
+                            v-model="cadastroEventoForm.tema" />
                         <span class="icon is-left"><i class="fa"></i></span>
                     </div>
                     <div class="columns is-four-fifths">
                         <div class="column is-9">
                             <label class="label">Local</label>
-                            <input class="input" type="text" placeholder="Email" name="firstName" />
+                            <input class="input" type="text" placeholder="Local" name="firstName"
+                                v-model="cadastroEventoForm.local" />
                         </div>
                         <div class="column">
-                            <label class="label" for="firstName">Telefone</label>
+                            <label class="label" for="firstName">Data Evento</label>
                             <div class="control">
                                 <div>
                                     <span class="icon">
                                         <i class="fas fa-calendar"></i>
                                     </span>
-                                    <flat-pickr class="button" :config="config" v-model="dataEvento"></flat-pickr>
+                                    <flat-pickr class="button" :config="config" v-model="cadastroEventoForm.dataEvento">
+                                    </flat-pickr>
                                 </div>
                             </div>
                         </div>
@@ -28,33 +31,41 @@
                     <div class="columns">
                         <div class="column"><label class="label" for="password">Senha</label>
                             <div class="control has-icons">
-                                <input class="input" type="new-password" placeholder="Password" name="password" />
+                                <input class="input" type="new-password" placeholder="Password" name="password"
+                                    v-model="cadastroEventoForm.passaword.password" />
                                 <span class="icon is-left"><i class="fa"></i></span>
                             </div>
                         </div>
                         <div class="column"><label class="label" for="retypePassword">Confirmar Senha</label>
-                            <div class="control has-icons-left"><input class="input" type="current-password"
-                                    placeholder="Confirm Password" name="retypePassword" /><span class="icon is-left"><i
-                                        class="fa"></i></span></div>
+                            <div class="control has-icons-left">
+                                <input class="input" type="current-password" placeholder="Confirm Password"
+                                    name="retypePassword" v-model="cadastroEventoForm.passaword.confirPassword" />
+                                <span class="icon is-left">
+                                    <i class="fa"></i>
+                                </span>
+                            </div>
                         </div>
                     </div>
                     <div class="columns">
-                        <div class="column is-1"><label class="label" for="password">Qtd Pessoas</label>
+                        <div class="column is-2"><label class="label" for="password">Qtd Pessoas</label>
                             <div class="control has-icons">
-                                <input class="input" type="number" name="password" />
+                                <input class="input" type="number" name="password"
+                                    v-model="cadastroEventoForm.qtdPessoas" />
                                 <span class="icon is-left"><i class="fa"></i></span>
                             </div>
                         </div>
                         <div class="column">
                             <label class="label" for="retypePassword">Telefone</label>
                             <div class="control has-icons-left">
-                                <input class="input" type="number" name="retypePassword" />
+                                <input class="input" type="number" name="retypePassword"
+                                    v-model="cadastroEventoForm.telefone" />
                                 <span class="icon is-left"><i class="fa"></i></span>
                             </div>
                         </div>
                         <div class="column"><label class="label" for="retypePassword">Email</label>
                             <div class="control has-icons-left">
-                                <input class="input" type="email" name="retypePassword" />
+                                <input class="input" type="email" name="retypePassword"
+                                    v-model="cadastroEventoForm.email" />
                                 <span class="icon is-left"><i class="fa"></i></span>
                             </div>
                         </div>
@@ -67,9 +78,15 @@
             <div class="field is-grouped ">
                 <div class="control  ">
                     <button class="button is-medium is-info"> Cancelar</button>
-                    <router-link to="/user/login" class="button is-primary is-medium" type="submit"> Salvar Alteração
-                    </router-link>
+                    <button class="button is-primary is-medium" type="submit" @click="cadastrar()"> Salvar Alteração
+                    </button>
                 </div>
+            </div>
+        </div>
+        <div :class="{ error: v$.$errors.length }">
+            <input v-model="cadastroEventoForm.tema">
+            <div class="input-errors" v-for="error of v$.$errors" :key="error.$uid">
+                <div class="error-msg">{{ error.$message }}</div>
             </div>
         </div>
     </div>
@@ -78,10 +95,51 @@
 
 <script setup lang="ts">
 import { reactive, ref } from "vue";
-const dataEvento = ref<Date>(new Date());
+import useVuelidate from '@vuelidate/core'
+import { required, email } from '@vuelidate/validators'
+
 const config = reactive({
     dateFormat: 'd-m-Y'
-})
+});
+const cadastroEventoForm = reactive({
+    tema: '',
+    local: '',
+    dataEvento: '',
+    passaword: {
+        password: '',
+        confirPassword: ''
+    },
+    qtdPessoas: 0,
+    telefone: '',
+    email: ''
+});
+
+const rules = {
+    tema: { required },
+    local: { required },
+    dataEvento: { required },
+    passaword: {
+        password: { required },
+        confirPassword: { required }
+    },
+    qtdPessoas: { required },
+    telefone: { required },
+    email: { required }
+}
+const v$ = useVuelidate(rules, cadastroEventoForm);
+
+function cadastrar() {
+    v$.value.$validate()
+    console.log("cadastroEventoForm", cadastroEventoForm);
+    console.log("v", v$.value.$error)
+    console.log("erros", v$.value.$errors)
+    if (!v$.value.$error)
+        alert("Form sucessfuly")
+    else
+        alert("Erro")
+}
+
+
 </script>
 
 <style lang="scss">
